4e9f828ec75eb79570b07bd228aa73a34bdd4335
AWM018 <awm018@interia.pl>
2019-05-30 20:15:39 +0200

    Make first test

--- generic_strategy_optimization/tests/test_transform.py
+++ generic_strategy_optimization/tests/test_transform.py
@@ -4,22 +4,29 @@
 """Tests for `transform` package."""
 
 import pytest
+import pandas as pd
+from pandas.testing import assert_series_equal
 
-
-from generic_strategy_optimization.transform import gen_HA
+from generic_strategy_optimization.transform import HA, gen_HA
 
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
+def candles_5m_short():
+    arr = [
+            [1504927800,303.11,305.1,305.02,304.39,452.56688104],
+            [1504928100,303.99,304.4,304.39,303.99,508.66859951],
+            [1504928400,303.04,304.4,303.99,303.88,526.7675571],
+          ]
+    rv = pd.DataFrame(arr, columns=('ts', 'low', 'high', 'open', 'close', 'volume'))
+    return rv
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
-
-def test_content(response):
+def test_HA_adds_heikin_ashi_column_to_dataframe(candles_5m_short):
     """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+    df = HA(candles_5m_short)
+    assert 'heikin_ashi' in df
+    assert len(df) == len(candles_5m_short)
+    assert_series_equal(df.heikin_ashi, pd.Series([304.405, 304.1925, 303.8275]), check_names=False)
+
+
