e6078d71b6ac95ac49b08b77e8ed6acd6dad3db7
AWM018 <awm018@interia.pl>
2019-05-30 23:53:01 +0200

    Add implementation w/ tests for coarse downsample

--- generic_strategy_optimization/generic_strategy_optimization/transform.py
+++ generic_strategy_optimization/generic_strategy_optimization/transform.py
@@ -3,6 +3,7 @@
 """Main module."""
 
 import pandas as pd
+import numpy as np
 
 def gen_HA(df : pd.DataFrame) -> pd.Series:
     ha = (df.close + df.open + df.high + df.low) / 4.
@@ -11,5 +12,32 @@ def gen_HA(df : pd.DataFrame) -> pd.Series:
 
 def HA(df : pd.DataFrame) -> pd.DataFrame:
     df['heikin_ashi'] = gen_HA(df)
+    return df
+
+
+def downsample(df: pd.DataFrame, N: int, method: str = 'coarse') -> pd.DataFrame:
+    assert method in ['fine', 'coarse']
+
+    if method == 'coarse':
+        L = df.low.rolling(N).apply(np.ndarray.min)
+        H = df.high.rolling(N).apply(np.ndarray.max)
+        O = df.open.rolling(N).apply(lambda a: a[0])
+        C = df.close.rolling(N).apply(lambda a: a[-1])
+        V = df.volume.rolling(N).apply(np.ndarray.sum)
+
+        ix = (len(df) + N - 1) % N
+        rv = pd.DataFrame.from_items([
+            #('ts', ts[ix::N]),
+            ('low', L[ix::N]),
+            ('high', H[ix::N]),
+            ('open', O[ix::N]),
+            ('close', C[ix::N]),
+            ('volume', V[ix::N]),
+        ]).dropna(axis=0)
+        rv.index = df.index[(len(df) % N)::N]
+
+        return rv
+    elif method == 'coarse':
+        pass
     return df
 
